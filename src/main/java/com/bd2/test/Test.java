1ed6f6ec0560e5cef57e085391497ec177fc65a9
leonardo <leonardo@NC-NOTEBOOK>
2014-11-26 14:46:24 -0300

    Primeiro Commit

---
+++ src/main/java/com/bd2/test/Test.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bd2.test;
+
+import com.bd2.entiies.Pessoa;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author leonardo
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory factory 
+                = Persistence.createEntityManagerFactory("netflixlab_pu");
+        EntityManager em = factory.createEntityManager();
+
+        
+        em.close();
+        factory.close();
+        
+
+    }
+
+}
